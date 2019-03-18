b940581a05f493d65427aabeb901ab2f9cd182f7
Tyrell Keene <tyrell.wkeene@gmail.com>
2019-03-18 15:08:01 -0600

    Updated example code

--- example/example.go
+++ example/example.go
@@ -15,48 +15,29 @@ func handleEvents(watcher *fsevents.Watcher) {
 	for {
 		select {
 		case event := <-watcher.Events:
-			log.Printf("Event Name: %s Event Path: %s", event.Name, event.Path)
+			log.Printf("Event Name: %s Event Path: %s Event Descriptor: %v", event.Name, event.Path, event.Descriptor)
+			log.Println("Watcher Event Count:", watcher.GetEventCount())
+			log.Println("Running descriptors:", watcher.GetRunningDescriptors())
 
-			// Root watch directory was deleted, panic
-			log.Printf("Watcher %s event %s", watcher.RootPath, event.Path)
-			if event.IsRootDeletion(watcher.RootPath) == true {
-				panic("Root watch directory deleted!")
-			}
-
-			// Directory events
 			if event.IsDirCreated() == true {
 				log.Println("Directory created:", path.Clean(event.Path))
-				if err := watcher.AddDescriptor(path.Clean(event.Path), 0); err != nil {
-					panic(err)
-				} else {
-					// When a new dir is created, we need to add it to the descriptors list and start it
-					descriptor := watcher.GetDescriptorByPath(path.Clean(event.Path))
-					descriptor.Start(watcher.FileDescriptor)
-					log.Printf("Watch descriptor created and started for %s", event.Path)
-				}
+				watcher.AddDescriptor(path.Clean(event.Path), 0)
+				descriptor := watcher.GetDescriptorByPath(path.Clean(event.Path))
+				descriptor.Start(watcher.FileDescriptor)
 			}
 			if event.IsDirRemoved() == true {
 				log.Println("Directory removed:", path.Clean(event.Path))
-				if err := watcher.RemoveDescriptor(path.Clean(event.Path)); err != nil {
-					panic(err)
-				} else {
-					log.Printf("Watch descriptor removed for %s", event.Path)
-				}
-			}
-			if event.IsDirChanged() == true {
-				log.Println("Directory changed: ", event.Name)
+				watcher.RemoveDescriptor(path.Clean(event.Path))
 			}
 
-			// File events
 			if event.IsFileCreated() == true {
 				log.Println("File created: ", event.Name)
 			}
 			if event.IsFileRemoved() == true {
 				log.Println("File removed: ", event.Name)
 			}
-			if event.IsFileChanged() == true {
-				log.Println("File changed: ", event.Name)
-			}
+			break
+
 			break
 		case err := <-watcher.Errors:
 			log.Println(err)
